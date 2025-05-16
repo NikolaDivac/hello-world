f4e434fd23112754c5e078d5b1e373475b6258d8
Nikola <nikoladivac3@gmail.com>
2025-05-16 14:18:41 +0200

    HelloWorld included

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Your name
+ * @Date:   2025-05-16 14:16:54
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2025-05-16 14:17:34
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
